d136661faea522bd18c16aa2732f7615f9571ad2
cmstrandwold <strandwold.c@husky.neu.edu>
2022-11-28 09:18:43 -0500

    changes

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const leaderModel = require('./model');
 const app = express();
 
 app.post("/leaderboard/add", async (request, response) => {
-    const leader = new leaderModel(request.body) || new leaderModel({"id": "ttt", "name": "test request", "wins": 11});
+    const leader = new leaderModel(request.body) || new leaderModel({"user_id": "ttt", "name": "test request", "wins": 11});
     try {
       await leader.save();
       leaderModel.updateOne(leader);
@@ -17,13 +17,13 @@ app.patch("/leaderboard/updateWins", async (request, response) => {
     const leader = new leaderModel(request.body)
     try {
         await leader.save();
-        const filter = { id: leader.id };
+        const filter = { user_id: leader.id };
         const options = { upsert: false };
         const updateDoc = { wins: 999 };
-        const result = await leaderModel.updateOne(filter, updateDoc, options);
-        console.log(
-          `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
-        );
+        const result = await leaderModel.updateOne(filter, updateDoc, options).then(result => console.log(
+            `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+          ));
+        
         response.send(leader);
     } catch (error) {
       response.status(500).send(error);
