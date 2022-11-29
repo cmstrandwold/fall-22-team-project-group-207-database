dc2870cff88cf1d01f64954e679e8c29bca75546
cmstrandwold <strandwold.c@husky.neu.edu>
2022-11-29 16:01:36 -0500

    added new endpoint for adding a win

--- routes.js
+++ routes.js
@@ -13,20 +13,43 @@ app.post("/leaderboard/add", async (request, response) => {
     }
 });
 
-app.patch("/leaderboard/updateWins", async (request, response) => {  
-    const leader = new leaderModel(request.body)
+app.patch("/leaderboard/update/{userid}", async (request, response) => {  
+  const leader = new leaderModel(request.body)
+  try {
+      await leader.save();
+      const filter = { user_id: leader.user_id };
+      const options = { upsert: false };
+      const updateDoc = { wins: leader.wins };
+      console.log('filtering')
+      console.log(await leaderModel.filter(filter));
+      const result = await leaderModel.updateOne(filter, updateDoc, options).then(result => console.log(
+          `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+        ));
+        console.log(result);
+      response.send(leader);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.patch("/leaderboard/win/{userid}", async (request, response) => {  
+    const leader_id = await request.params['userid']
     try {
+        await leader_id.save();
+        const leader = leaderModel.find({user_id: leader_id})
         await leader.save();
-        const filter = { user_id: leader.user_id };
+        const filter = { user_id: leader_id };
         const options = { upsert: false };
-        const updateDoc = { wins: leader.wins };
+        const newWins = leader.wins + 1;
+        const updateDoc = { wins: newWins };
         console.log('filtering')
         console.log(await leaderModel.filter(filter));
         const result = await leaderModel.updateOne(filter, updateDoc, options).then(result => console.log(
             `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
           ));
           console.log(result);
-        response.send(leader);
+        await result.save();
+        response.send(result);
     } catch (error) {
       response.status(500).send(error);
     }
